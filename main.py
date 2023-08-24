1ca5a63defaf6b267b6c3f2a9694c0d28ae0e8a5
Nikolas <subedikiran678@gmail.com>
2023-08-24 12:50:25 +0545

    shoe task

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ class ToDoList:
         if 0 <= task_index < len(self.tasks):
             self.tasks[task_index]["completed"] = True
 
-   
+    def show_tasks(self):
+        task_data = []
+        for index, task in enumerate(self.tasks):
+            status = "✓" if task["completed"] else " "
+            task_data.append([index + 1, status, task['task']])
+
         
 
 
