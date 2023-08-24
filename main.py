d9e05bcfd36145b0aee0e2b11320d68bdb0fb3d0
Nikolas <subedikiran678@gmail.com>
2023-08-24 12:50:09 +0545

    add task

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ class ToDoList:
     def __init__(self):
         self.tasks = []
 
-    
+    def add_task(self, task):
+        self.tasks.append({"task": task, "completed": False})
+
    
         
 
