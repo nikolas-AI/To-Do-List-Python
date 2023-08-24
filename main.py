4fa6a2b7e56586d394cde971a8b43a89fa367e87
Nikolas <subedikiran678@gmail.com>
2023-08-24 12:50:18 +0545

    mark completed

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class ToDoList:
     def add_task(self, task):
         self.tasks.append({"task": task, "completed": False})
 
+    def mark_completed(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index]["completed"] = True
+
    
         
 
